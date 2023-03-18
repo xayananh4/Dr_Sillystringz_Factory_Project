4d4c7ec91e62e79e03c603d9c19064f48d42efaf
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-17 18:05:28 -0700

    add EngineersController and index view

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -6,8 +6,10 @@ namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
-   
-
-  
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
   }
 }
